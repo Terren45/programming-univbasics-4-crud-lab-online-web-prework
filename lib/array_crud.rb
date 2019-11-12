59410c6c9dd0469e72434032742180c40da3a4ba
Terren Culberson <culbersonterren@yahoo.com>
2019-11-12 01:17:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
  ["cars", "truck", "planes", "trains"] 
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array=["cars", "truck", "planes", "trains"]
  [] << "boat"
 end
 
