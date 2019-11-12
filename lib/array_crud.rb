36f84e49b3d7b30075f9cf068eb77b5083ca624f
Terren Culberson <culbersonterren@yahoo.com>
2019-11-12 01:20:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,8 +10,8 @@ def add_element_to_end_of_array = ["cars", "truck", "planes", "trains"]
  add_element_to_end_of_array << "boat"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array["cars", "truck", "planes", "trains"]
+  add_element_to_start_of_array 
 end
 
 def remove_element_from_end_of_array(array)
